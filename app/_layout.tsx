bc92cf0d69059314ef0a5a9e892d841e66417c4d
QPM <admin@QuantumPinnacleMatrix.tech>
2024-05-23 20:41:49 +0300

    done with auth page UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,24 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { TouchableOpacity } from "react-native";
 
 export default function RootLayout() {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
         name="login"
-        options={{ headerShown: false, presentation: "modal" }}
+        options={{
+          title: "",
+          presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
       />
     </Stack>
   );
--- app/login.tsx
+++ app/login.tsx
@@ -1,12 +1,112 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
+  const { type } = useLocalSearchParams<{ type: string }>();
+  const [loading, setLoading] = useState(false);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const onSignUpPress = async () => {};
+  const onSignInPress = async () => {};
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={70}
+      style={[styles.container]}
+    >
+      {loading && (
+        <View style={defaultStyles.loadingOverlay}>
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+      <Image
+        source={require("../assets/images/logo-dark.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>
+        {type === "login" ? "Welcome back" : "Create your account"}
+      </Text>
+      <View style={{ marginBottom: 30 }}>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Email"
+          style={styles.inputField}
+          value={emailAddress}
+          onChangeText={setEmailAddress}
+        />
+        <TextInput
+          autoCapitalize="none"
+          placeholder="Password"
+          style={styles.inputField}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
+      {type === "login" ? (
+        <TouchableOpacity
+          onPress={onSignInPress}
+          style={[defaultStyles.btn, styles.btnPrimary]}
+        >
+          <Text style={styles.btnPrimaryText}>Login</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          onPress={onSignUpPress}
+          style={[defaultStyles.btn, styles.btnPrimary]}
+        >
+          <Text style={styles.btnPrimaryText}>Create Account</Text>
+        </TouchableOpacity>
+      )}
+    </KeyboardAvoidingView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    alignSelf: "center",
+    marginVertical: 80,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: Colors.primary,
+    borderRadius: 12,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  btnPrimary: {
+    backgroundColor: Colors.primary,
+    marginVertical: 4,
+  },
+  btnPrimaryText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
 export default Page;
