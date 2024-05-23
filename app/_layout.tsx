8d4085049561e1f489504b216685154453f163a3
QPM <admin@QuantumPinnacleMatrix.tech>
2024-05-23 20:19:59 +0300

    done with landing page start working on auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="login"
+        options={{ headerShown: false, presentation: "modal" }}
+      />
     </Stack>
   );
 }
