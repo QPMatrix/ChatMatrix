f14a2ca8090c5a322052f1c8866711e2fda25102
QPM <admin@QuantumPinnacleMatrix.tech>
2024-05-28 10:58:24 +0300

    done with input ui

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,13 +1,23 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React from "react";
 import { useAuth } from "@clerk/clerk-expo";
 import { defaultStyles } from "@/constants/Styles";
 import { Stack } from "expo-router";
 import HeaderDropDown from "@/components/header-drop-down";
+import MessageInput from "@/components/message-input";
 
 const Page = () => {
   const { signOut } = useAuth();
   const [gptVersion, setGptVersion] = React.useState("3.5");
+  const getCompletion = async (message: string) => {
+    console.log("Message:", message);
+  };
   return (
     <View style={defaultStyles.pageContainer}>
       <Stack.Screen
@@ -33,7 +43,22 @@ const Page = () => {
           ),
         }}
       />
-      <Button title="Sign Out" onPress={() => signOut()} />
+      <View style={{ flex: 1 }}>
+        <Text>Dummy Content</Text>
+        <Button title="Sign Out" onPress={() => signOut()} />
+      </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={70}
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+        }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <MessageInput onShouldSend={getCompletion} />
+      </KeyboardAvoidingView>
     </View>
   );
 };
